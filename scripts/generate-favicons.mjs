46534693c033a501e7b91ef122dc08a5741f58b7
Justin Napolitano <napolitano.just@gmail.com>
2026-01-18 21:53:15 +0100

    added some more icons

--- scripts/generate-favicons.mjs
+++ scripts/generate-favicons.mjs
@@ -11,12 +11,14 @@ const sourceSvg = path.join(publicDir, 'favicon.svg');
 const pngTargets = [
   { size: 16, name: 'favicon-16x16.png' },
   { size: 32, name: 'favicon-32x32.png' },
+  { size: 48, name: 'favicon-48x48.png' },
   { size: 180, name: 'apple-touch-icon.png' },
+  { size: 180, name: 'apple-touch-icon-precomposed.png' },
   { size: 192, name: 'android-chrome-192x192.png' },
   { size: 512, name: 'android-chrome-512x512.png' },
 ];
 
-const icoSizes = [16, 32];
+const icoSizes = [16, 32, 48];
 
 async function ensureSourceExists() {
   try {
